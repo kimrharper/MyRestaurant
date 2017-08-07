8fb3f64eb3e96add7530bbbc77409f7f59eddcc1
Ryan Harper <kimrharper@gmail.com>
2017-08-07 14:25:58 -0700

    Initial commit

---
+++ app/src/main/java/com/example/ryanmain/myrestaurant/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ryanmain.myrestaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
